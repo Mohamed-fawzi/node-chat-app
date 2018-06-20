4f4b798b5dc3baa60e8423ccef3c47ecfa8ebcc2
Mohamed-fawzi <mohamed5283299@gmail.com>
2018-06-20 06:03:29 +0200

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket = io();
+socket.on('connect', function() {
+   console.log('Connected to server');
+
+   socket.emit('createMessage', {
+     from: 'user-4',
+     text: 'Hey, This is user-1'
+   });
+ });
+socket.on('disconnect', function() {
+   console.log('Disconnected from server')
+ });
+
+socket.on('newMessage', function(message) {
+  console.log('New Message!', message);
+});
